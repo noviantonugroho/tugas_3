adad3cf1fc4611ab57910f08088981b99cba2c4b
noviantonugroho <noviantonugroho1989@gmail.com>
2014-12-04 10:55:51 +0700

    first commit

---
+++ app/models/article.rb
@@ -0,0 +1,15 @@
+class Article < ActiveRecord::Base
+	validates :title, presence: true,
+
+    	length: { minimum: 5 }
+
+    validates :content, presence: true,
+
+        length: { minimum: 10 }
+
+    validates :status, presence: true
+
+    scope :status_active, -> {where(status: 'active')}
+
+    default_scope {where(status: 'active')}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails1::Application.config.secret_key_base = '58aa16a19e3ef3841478f279e7d5fa8101bc059849431364c8a0b6622541732435e84ef4b3edc21a0fee6f50ed726dffac7d50af3bbb73088d7b0eef16e590e6'
